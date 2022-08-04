041587cc8978744e13e6992a412b4d322a532bd8
SchrodingersSangru <deshpande.sangram156@gmail.com>
2022-08-04 19:25:07 +0530

    completed the project

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '-95t%=#4o3@l-(-%ok9*h%n3!0(sdchjn%+_$5#umaj-!3bg*7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -122,3 +122,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = "/image/download/"
 MEDIA_ROOT = BASE_DIR
+### adding the store ###
